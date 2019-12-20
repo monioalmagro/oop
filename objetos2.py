cb43da8670c59a7a6ebd7aebf1759258912fc0f0
almagro <emazzurque@gmail.com>
2019-12-20 00:08:50 -0300

    primer commit

--- objetos2.py
+++ objetos2.py
@@ -7,9 +7,9 @@ atributos, imprimirlos y mostrar un mensaje si está regular
 
 Definir dos objetos de la clase Alumno."""
 
-class Alumno:
+class Alumno:#--> de esta manera declaramos la clase persona.
 
-	def inicializar(self,nombre,nota):
+	def inicializar(self,nombre,nota):#--> esta es la manera de declarar un método de la clase.
 		self.nombre = nombre
 		self.nota = nota
 
@@ -17,12 +17,23 @@ class Alumno:
 		print("Nombre: ",self.nombre)
 		print("Nota: ",self.nota)
 
-	def regular(self):
+	def mostrar_estado(self):
 		if self.nota >= 4:
 			print("Alumno regular.")
 		else:
-			print("Alumno no regular.")	
+			print("Libre.")	
 
 #bloque
+
+alumno1 = Alumno()#-->Instanciamos la clase Persona 
+alumno1.inicializar("Sinisterra", 10)
+alumno1.imprimir()
+alumno1.mostrar_estado() 
+
+alumno2 = Alumno()
+alumno2.inicializar("Cuartas", 3)
+alumno2.imprimir()
+alumno2.mostrar_estado() 
+
 
 
