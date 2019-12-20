a720c26656b3f77ae0cfc646c26a30a2cb0a8493
almagro <emazzurque@gmail.com>
2019-12-19 23:50:32 -0300

    primer commit

---
+++ objetos2.py
@@ -0,0 +1,8 @@
+"""Problema 2:
+
+Implementar una clase llamada Alumno que tenga como atributos 
+su nombre y su nota. Definir los métodos para inicializar sus 
+atributos, imprimirlos y mostrar un mensaje si está regular 
+(nota mayor o igual a 4)
+
+Definir dos objetos de la clase Alumno."""
