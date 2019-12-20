8b114eb633b3e5a81c2042f61ac53b2441e16e28
almagro <emazzurque@gmail.com>
2019-12-20 00:23:09 -0300

    primer commit

--- objetos3.py
+++ objetos3.py
@@ -11,3 +11,27 @@ class Persona:
 	def inicilizar(self,nombre,edad):
 		self.nombre = nombre
 		self.edad = edad
+
+	def imprimir(self):
+		print("El nombre es :",self.nombre)
+		print("Y su edad es : ", self.edad)
+
+	def mostrar_mayor(self):
+		if self.edad >= 18:
+			print(self.nombre, " es mayor de edad.")
+		else:
+			print(self.nombre, " es menor de edad.")
+
+#Bloque principal
+
+persona1 = Persona()
+persona1.inicilizar("Yaque", 34)
+persona1.imprimir()
+persona1.mostrar_mayor()
+
+persona1 = Persona()
+persona1.inicilizar("Figueroa", 10)
+persona1.imprimir()
+persona1.mostrar_mayor()
+
+
