3f2edfc397b71d37eab1c7b1fafcb1f413523059
almagro <emazzurque@gmail.com>
2019-12-19 23:41:37 -0300

    primer commit

---
+++ objetos1.py
@@ -0,0 +1,18 @@
+"""Problema 1:
+
+Implementaremos una clase llamada Persona que tendrá como 
+atributo (variable) su nombre y dos métodos (funciones), 
+uno de dichos métodos inicializará el atributo nombre y el 
+siguiente método mostrará en la pantalla el contenido del mismo.
+
+Definir dos objetos de la clase Persona."""
+
+
+class Persona:
+
+	def inicializar(self,nom):
+		self.nombre = nom
+
+	def imprimir(self):
+		print("Nombre",self.nombre)	
+		
