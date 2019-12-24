0709fd654d1b6e495f9f16018f5fb87705bab564
almagro <emazzurque@gmail.com>
2019-12-23 22:01:30 -0300

    ejercicios

--- objetos4.py
+++ objetos4.py
@@ -8,18 +8,38 @@ si es equilátero o no. El nombre de la clase llamarla Triangulo."""
 
 class Triangulo:
 
-	def inicializar(self,lado1,lado2,lado3):
-		self.lado1 = lado1
-		self.lado2 = lado2
-		self.lado3 = lado3
+    def inicializar(self):
+        self.lado1=int(input("Ingrese primer lado:"))
+        self.lado2=int(input("Ingrese segundo lado:"))
+        self.lado3=int(input("Ingrese tercer lado:"))
 
-	def equilatero(self):
-		if self.lado1 == self.lado2 == self.lado3 :
-			print ("es un triangulo Equilátero")
+    def imprimir(self):
+        print("Valores de los lados del triangulo")
+        print("Lado 1",self.lado1)
+        print("Lado 2",self.lado2)
+        print("Lado 3",self.lado3)
+
+    def lado_mayor(self):
+        print("Lado mayor")
+        if self.lado1>self.lado2 and self.lado1>self.lado3:
+            print(self.lado1)
+        else:
+            if self.lado2>self.lado3:
+                print(self.lado2)
+            else:
+                print(self.lado3)
+
+    def es_equilatero(self):
+        if self.lado1==self.lado2 and self.lado1==self.lado3:
+            print("El triangulo es equilatero")
+        else:
+            print("El triangulo no es equilatero")
 
 
-triangulo=Triangulo()
-triangulo.inicializar(4,4,4)
-input("Ingrese una tecla:")
-triangulo.equilatero()
+# bloque principal
 
+triangulo1=Triangulo()
+triangulo1.inicializar()
+triangulo1.imprimir()
+triangulo1.lado_mayor()
+triangulo1.es_equilatero()
