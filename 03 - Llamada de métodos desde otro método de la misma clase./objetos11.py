8533e7ff6bec2a72d44b52dbdc6461113c7f5c29
almagro <emazzurque@gmail.com>
2019-12-26 01:15:08 -0300

    Llamada de métodos

---
+++ 03 - Llamada de métodos desde otro método de la misma clase./objetos11.py
@@ -0,0 +1,8 @@
+"""Confeccionar una clase que administre una agenda personal. 
+Se debe almacenar el nombre de la persona, teléfono y mail
+Debe mostrar un menú con las siguientes opciones:
+1- Carga de un contacto en la agenda.
+2- Listado completo de la agenda.
+3- Consulta ingresando el nombre de la persona.
+4- Modificación de su teléfono y mail.
+5- Finalizar programa. """
