0d9e3a004627190b59583d3e69f66edd03cc17e7
almagro <emazzurque@gmail.com>
2019-12-30 00:46:37 -0300

    Colaboracion de clases

--- 04 - Colaboración de clases./objetos14.py
+++ 04 - Colaboración de clases./objetos14.py
@@ -16,28 +16,28 @@ class Socio:
 	def imprimir(self):
 		print("El Socio: ",self.nombre,"tiene: ",self.anti,"años de antigüedad")	
 
-	def retornar_datos:
+	def retornar_antiguedad(self):
 		return self.nombre,self.anti
 
-class club:
+class Club:
 	
 	def __init__(self):
 		self.socio1=Socio()
 		self.socio2=Socio()
 		self.socio3=Socio()
-
+	
 	def mayor_antiguedad(self):
-        print("Socio con mayor antiguedad")
-        if (self.socio1.retornar_antiguedad()>self.socio2.retornar_antiguedad() and
-            self.socio1.retornar_antiguedad()>self.socio3.retornar_antiguedad()):
-            self.socio1.imprimir()
-        else:
-            if self.socio2.retornar_antiguedad()>self.socio3.retornar_antiguedad():
-                self.socio2.imprimir()
-            else:
-                self.socio3.imprimir()
-    
-#
+
+		print("Socio con mayor antiguedad: ")
+		if (self.socio1.retornar_antiguedad()>self.socio2.retornar_antiguedad() and
+			self.socio1.retornar_antiguedad()>self.socio3.retornar_antiguedad()):
+			self.socio1.imprimir()
+		else:
+			if self.socio2.retornar_antiguedad()>self.socio3.retornar_antiguedad():
+				self.socio2.imprimir()
+			else:
+				self.socio3.imprimir()	
+
 club=Club() 
 club.mayor_antiguedad()            	
 
