9b850899e4edbe539e48a24a5f026a8bac535c06
almagro <emazzurque@gmail.com>
2019-12-29 22:15:17 -0300

    colaboración dce

--- 04 - Colaboración de clases./objetos12.py
+++ 04 - Colaboración de clases./objetos12.py
@@ -2,3 +2,22 @@
 También el banco requiere que al final del día calcule la cantidad de dinero 
 que hay depositado."""
 
+class Cliente:
+	def __init__(self,nombre):
+		self.nombre = nombre
+		self.monto = 0
+
+	def depositar(self, monto):
+		self.monto = self.monto + monto
+
+	def extraer(self,monto)	:
+		self.monto = self.monto - monto
+
+	def retornar_monto(self):
+		return self.monto
+
+	def imprimir(self):
+		print(self.nombre," tiene depositado la suma de : ",self.monto)
+
+			
+
