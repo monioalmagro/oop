b5f3dac2e0459fd09f84499cf925f0b09073d716
almagro <emazzurque@gmail.com>
2020-01-01 19:08:36 -0300

    Herencia

--- 05-Herencia/objetos16.py
+++ 05-Herencia/objetos16.py
@@ -18,8 +18,51 @@ La relación de herencia que podemos disponer para este problema es:
 
 Solamente el método operar es distinto para las clases Suma y Resta 
 (esto hace que no lo podamos disponer en la clase Operacion en principio), 
-luego los métodos cargar1, cargar2 y mostrar_resultado son idénticos a las 
+luego los mtodos cargar1, cargar2 y mostrar_resultado son idnticos a las 
 dos clases, esto hace que podamos disponerlos en la clase Operacion. Lo mismo 
-los atributos valor1, valor2 y resultado se definirán en la clase padre 
+los atributos valor1, valor2 y resultado se definirn en la clase padre 
 Operacion.
 """
+class Operacion:
+	def __init__(self):
+		self.valor1=0
+		self.valor2=0
+		self.resultado=0
+
+	def cargar1(self):
+		self.valor1=int(input("Ingrese el primer valor: "))	
+
+	def cargar2(self):
+		self.valor2=int(input("Ingrese el segundo valor: "))
+
+	def mostrar_resultado(self):
+		print(self.resultado)	
+
+	def operar(self):
+		pass	
+
+class Suma(Operacion):
+
+	def operar(self):			
+		self.resultado = self.valor1 + self.valor2
+
+class Resta(Operacion):
+	
+	def operar(self):
+		self.resultado = self.valor1 - self.valor2
+
+# bloque princpipal
+
+suma1=Suma()
+suma1.cargar1()
+suma1.cargar2()
+suma1.operar()
+print("La suma de los dos valores es")
+suma1.mostrar_resultado()
+
+resta1=Resta()
+resta1.cargar1()
+resta1.cargar2()
+resta1.operar()
+print("La resta de los valores es:")
+resta1.mostrar_resultado()				
