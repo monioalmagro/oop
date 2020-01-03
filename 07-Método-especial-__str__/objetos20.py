fcbb876e5b98325863fdde310f7cbc4d6846e2ed
almagro <emazzurque@gmail.com>
2020-01-03 00:43:11 -0300

    __str__

---
+++ 07-Método-especial-__str__/objetos20.py
@@ -0,0 +1,18 @@
+"""Definir una clase llamada Punto con dos atributos x e y.
+Crearle el método especial __str__ para retornar un string con el 
+formato (x,y)."""
+
+class Punto:
+	def __init__(self,x,y):
+		self.x=x
+		self.y=y
+
+	def __str__(self):
+		return "("+str(self.x)+","+str(self.y)+")"
+
+# bloque principal
+
+punto1=Punto(10,3)
+punto2=Punto(3,4)
+print(punto1)
+print(punto2)			
