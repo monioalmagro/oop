5efc0c74ced184f591282ee32f7b9ebaf31ee921
almagro <emazzurque@gmail.com>
2020-01-09 22:17:38 -0300

    change folder

---
+++ 09 - Redefinición de los operadores relacionales con objetos./objetos24.py
@@ -0,0 +1,5 @@
+"""Crear una clase Persona que tenga como atributo el nombre y la edad.
+El operador == retornará verdadero si las dos personas tienen la misma edad, 
+el operador > retornará True si la edad del objeto de la izquierda tiene una 
+edad mayor a la edad del objeto de la derecha del operador >, y así 
+sucesivamente """
