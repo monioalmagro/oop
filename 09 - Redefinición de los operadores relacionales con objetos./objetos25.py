86e87579781726cf6a14f96c61cc5c51724fefe8
almagro <emazzurque@gmail.com>
2020-01-09 22:23:38 -0300

    change folder

---
+++ 09 - Redefinición de los operadores relacionales con objetos./objetos25.py
@@ -0,0 +1,5 @@
+"""Plantear una clase Rectangulo. Definir dos atributos (ladomenor y 
+ladomayor). Redefinir el operador == de tal forma que tengan en cuenta la 
+superficie del rectángulo. Lo mismo hacer con todos los otros operadores 
+relacionales.
+"""
